6302244e3e72470e676786087964a53fe423dcf4
m-ramzan-dev <mhdramzan.dev@gmail.com>
2026-06-14 16:37:01 +0500

    search agents woworking

--- search.py
+++ search.py
@@ -2,28 +2,25 @@ from langchain_ollama import ChatOllama
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain.agents import create_agent
+from langchain_tavily import TavilySearch
+from dotenv import load_dotenv
 
+load_dotenv()
 
-@tool
-def search(query: str) -> str:
-    """
-    Search for information about a given query.
-    Args:        query (str): The search query.
-    Returns:        str: The search results.
-    """
-    return "Multan weather is sunny with a high of 30°C and a low of 20°C. No precipitation expected."
-
-
+tavily_search_tool = TavilySearch(
+    max_results=5,
+    topic="general"
+)
 #llm = ChatOllama(model="gemma3:4b", temperature=0.0)
 llm = ChatOllama(model="llama3.1:8b", temperature=0.0)
-tools = [search]
+tools = [tavily_search_tool]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain!")
     try:
-        response = agent.invoke({"messages": [HumanMessage(content="What's the weather like in Multan?")]})
+        response = agent.invoke({"messages": [HumanMessage(content="Find most recent jobs for 'Flutter Developer' on upwork")]})
         print(response)
     except Exception as e:
         print(f"An error occurred: {e}")
